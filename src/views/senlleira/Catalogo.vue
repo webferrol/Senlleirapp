91423995e25478a1f89bae8e09835c48ac2edafa
martin <gagochorenmartin@gmail.com>
2022-06-30 13:48:08 +0200

    ficha tecnica, vista admin especies

--- src/views/senlleira/Catalogo.vue
+++ src/views/senlleira/Catalogo.vue
@@ -1,10 +1,16 @@
 <template>
     <FiltroBusquedaVue></FiltroBusquedaVue>
-    <CatalogoUsuarioVue></CatalogoUsuarioVue>
+    <CatalogoUsuarioVue @mostrarFicha="mostrarFichaTecnica = true"></CatalogoUsuarioVue>
+    <FichaTecnicaVue v-if="mostrarFichaTecnica" @cerrarFicha="mostrarFichaTecnica = false"></FichaTecnicaVue>
 </template>
 
 <script setup>
+import {ref} from 'vue';
 import CatalogoUsuarioVue from '@/components/catalogo/CatalogoUsuario.vue';
 import FiltroBusquedaVue from '../../components/catalogo/FiltroBusqueda.vue';
+import FichaTecnicaVue from '../../components/catalogo/FichaTecnica.vue';
+
+
+const mostrarFichaTecnica = ref(false)
 
 </script>
