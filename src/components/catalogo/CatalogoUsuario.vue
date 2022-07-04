3a37483e81e5aa1f416b688e233f402f5fbe4ae0
martin <gagochorenmartin@gmail.com>
2022-07-04 11:03:58 +0200

    ficha tecnica catalogo, arreglado emits de senlleiras, storis de parques

--- src/components/catalogo/CatalogoUsuario.vue
+++ src/components/catalogo/CatalogoUsuario.vue
@@ -1,9 +1,8 @@
 <template>
-    <!-- <span class="background-color"></span> -->
     <div class="catalogo-section-component">
         <div class="arbol-catalogo-element" data-titulo="Mostrar" title="Máis info"
             v-for="(senlleira, index) in storeSenlleira.senlleiras" :key="index" identificador=senlleira.id
-            @click="mostrarFicha(senlleira); $emit('mostrarFicha') ">
+            @click="cargarDatosFicha(senlleira)">
             <div class="content-img">
                 <img alt="imagen del arbol senlleiro" :src="senlleira.google_url">
             </div>
@@ -20,43 +19,56 @@
         </div>
     </div>
      
+
+    <FichaTecnicaVue 
+    v-if="mostrarFicha" @cerrarFicha="mostrarFicha = false"
+    :nombre_arbol="fichaDatos.nombre_arbol" :images="imagenesFichaTecnica"></FichaTecnicaVue>
+
+     
 </template>
 
 <script setup>
 
 //Dependencias
-import "@/assets/css/catalogo/catalogo.css";
+import { ref } from "vue";
 import { useStoreSenlleiras } from "@/stores/senlleiras";
 import { useStoreEspecies } from "../../stores/especies";
-import { ref } from "vue";
+import FichaTecnicaVue from "./FichaTecnica.vue";
+
+import "@/assets/css/catalogo/catalogo.css";
 
 const storeSenlleira = useStoreSenlleiras();
 const storeEspecies = useStoreEspecies();
 storeSenlleira.setSenlleiras();
 storeEspecies.setEspecies();
 
-const props = defineProps({
-    fichaDatos: {
-        type: Array,
-        default: []
-    },
-    imagenesFichaTecnica: {
-        type: Array,
-        default: []
-    }
-})
-// Funcion para cargar las imagenes correspondientes de la ficha tecnica
-const mostrarFicha = async (objeto) => {
+
+
+const mostrarFicha = ref(false)
+const fichaDatos = ref(null);
+const imagenesFichaTecnica = ref([]);
+
+// Función que sirve para limpiar el array de imagenes
+const imagenesFichaTecnicaVaciar = () => {
+    while(imagenesFichaTecnica.value.length > 0)
+    imagenesFichaTecnica.value.pop()
+}
+
+// Funcion para cargar datos de la ficha y sus respectivas imágenes
+const cargarDatosFicha = async (objeto) => {
+    mostrarFicha.value = true;
+    // Limpiamos y cargamos los datos tecnicos de la ficha
     const especie = storeEspecies.especies.find(el=>el.id,objeto.idEspecie);
-    props.fichaDatos.pop();
-    props.fichaDatos.push({...objeto,...especie});
-    props.imagenesFichaTecnica.pop();
+    fichaDatos.value = null;
+    fichaDatos.value= {...objeto,...especie};
+    // Limpiamos y cargamos las imagenes de la ficha
+    imagenesFichaTecnicaVaciar()
     await storeSenlleira.setImagenes('senlleiras/' + objeto.id)
     for (let i = 0; i < storeSenlleira.imagenes.length; i++) {
-        props.imagenesFichaTecnica.push(storeSenlleira.imagenes[i])
+        imagenesFichaTecnica.value.push(storeSenlleira.imagenes[i])
     }
     await storeEspecies.setEspecies()
 
-    // console.log("datos son  => ",props.fichaDatos, "imagenes son => ",props.imagenesFichaTecnica)
+    console.log("datos son  => ",fichaDatos, "imagenes son => ",imagenesFichaTecnica.value)
 }
 </script>
