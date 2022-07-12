1f27a2abbae30b349f63957d59d9efdbb56b5201
xurxogonzalez <xurxo@webferrol.com>
2022-07-12 12:46:27 +0200

    retocado ficheros

--- src/views/parques/FichaParqueView.vue
+++ src/views/parques/FichaParqueView.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div class="erro" v-if="error.errorBool">
-      ({{ error.code }}) {{ error.message }}
-    </div>
-    <FichaParqueComponent :parque="parque" :images="imagenesFichaTecnica"></FichaParqueComponent>    
+      <div class="erro" v-if="error.errorBool">
+        ({{ error.code }}) {{ error.message }}
+      </div>
+    <FichaParqueComponent v-else 
+    :parque="parque" 
+    :images="imagenesFichaTecnica"
+    :arbores="arbores" 
+    :mapas="mapas" ></FichaParqueComponent>    
   </div>
 </template>
 
@@ -11,10 +15,11 @@
 import { useRoute } from "vue-router";
 import { ref } from "vue";
 import { listAllUrls } from "../../hook/storage.hook";
-import { getDocument } from "../../hook/firestore.hook";
+import { getDocument,busquedaDatos } from "../../hook/firestore.hook";
 import FichaParqueComponent from "../../components/parques/FichaParqueComponent.vue"
 const route = useRoute();
 const parque = ref({});
+const arbores = ref([]);
 const error = ref({
   errorBool: false,
   code: 0,
@@ -22,6 +27,7 @@ const error = ref({
 });
 
 const imagenesFichaTecnica = ref([]);
+const mapas = ref([]);
 
 /**
  * CARGAMOS LA IMFORMACIÓN DEL PARQUE
@@ -30,16 +36,16 @@ const imagenesFichaTecnica = ref([]);
   try {
     error.value.errorBool = false;
     parque.value = await getDocument("Parques", route.params.idDoc);
+    arbores.value = await busquedaDatos("arbores","idParque",route.params.idDoc);
     if (!parque.value)
       throw new Error(
         `El parque con código ${route.params.idDoc} no existe. Fichero FichaParqueView.vue`
       );
     //Carga de imágenes
 
-    imagenesFichaTecnica.value = await listAllUrls(
-      "parques/" + route.params.idDoc
-    );
-    //console.log(imagenesFichaTecnica.value)
+    imagenesFichaTecnica.value = await listAllUrls("parques/" + route.params.idDoc);
+    mapas.value = await listAllUrls("parquesficha/",route.params.idDoc);
+    //console.log(mapas.value)
   } catch (err) {
     error.value.errorBool = true;
     error.value.code = err.code;
