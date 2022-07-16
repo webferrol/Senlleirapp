53cb3e2d304bf12d820595f9e243106d61161dc7
webferrol <xurxo@webferrol.com>
2022-07-16 15:58:34 +0200

    CSS de noticias y obtención con consulta del árbol de conxo

--- src/components/componentesGenerales/NoticiaCarballo.vue
+++ src/components/componentesGenerales/NoticiaCarballo.vue
@@ -4,11 +4,22 @@
     <div class="noticia-block-txt">
         <h1>Carballo banquete de conxo</h1>
         <h3>Gañador árbore nacional 2021</h3>
-        <router-link to="#">Ver árbore</router-link>
+        <router-link :to="{
+            name:'FichaSenlleira',
+            params:{'idDoc':'VDipVrgnJqjKrJQzn2Gc'},
+        }">Ver árbore</router-link>
     </div>
 </div>
 </template>
 
 <script setup>
+//Descomenta estas líneas si deseas encontrar el código en la BBDD del ID del árbol de conxo o búsca este id en firestore en su base de datos
+//import {busquedaDatos} from "../../hook/firestore.hook";
 import('@/assets/css/componente/noticia-arbol.css');
+
+// (async()=>{
+//     const res = await busquedaDatos("Arbores","nombre_arbol","Carballo do Banquete de Conxo");
+//     console.log(res[0].idDoc);
+// })()
+
 </script>
