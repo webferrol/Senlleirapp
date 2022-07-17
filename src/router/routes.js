6b3215f2a986ef94708d861458ab1e1091b4ffc0
webferrol <xurxo@webferrol.com>
2022-07-17 15:05:12 +0200

    Eliminado ficheros innecesarios y retocado las rutas del routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,38 +7,74 @@ export const routes = [
   {
     path: "/nova-arbore",
     name: "NuevoArbol",
+    meta: {
+      title: 'Engadir árbore',
+    },
     component: () => import("@/views/senlleira/NuevoArbol.vue"),
   },
   {
     path: "/catalogo",
     name: "catalogo",
+    meta: {
+      title: 'Catálogo de árbores',
+    },
     component: () => import("@/views/senlleira/CatalogoArboresView.vue"),
   },
   {
     path: "/catalogo-senlleiras",
     name: "CatalogoSenlleiras",
+    meta:{
+      title: 'Catálogo de Senlleiras'
+    },
     component: () => import("@/views/senlleira/CatalogoSenlleirasView.vue"),
   },
-  // {
-  //   path: "/mapaParques",
-  //   name: "MapaParques",
-  //   component: () => import("@/views/parques/MapaParques.vue"),
-  // },
-  
-  // {
-  //   path: "/mapaSenlleiras",
-  //   name: "MapaSenlleiras",
-  //   component: () => import("@/views/senlleira/MapaSenlleiras.vue"),
-  // },
   {
     path: "/mapa-parques",
     name: "MapaParques",
+    meta:{
+      title: 'Mapa dos parques',
+    },
     component: () => import("@/views/parques/ParkMapView.vue"),
   },
   {
     path: "/mapa-senlleiras",
     name: "MapaSenlleiras",
+    meta:{
+      title: 'Catálogo de Senlleiras',
+    },
     component: () => import("@/views/senlleira/SenlleiraMapView.vue"),
+  },
+  {
+    path: "/catalogo-de-especies",
+    name: "CatalogoDeEspecies",
+    meta: {
+      title: 'Especies',
+    },
+    component: () => import("@/views/especies/CatalogoEspecies.vue"),
+  },
+  {
+    path: "/catalogo-de-parques",
+    name: "CatalogoDeParques",
+    meta:{
+      title: 'Catálogo de parques',
+    },
+    component: () => import("@/views/parques/CatalogoParques.vue"),
+  },
+  {
+    path: "/ficha-parque/:idDoc",
+    name: "FichaParque",
+    meta:{
+      title: 'Ficha do parque',
+    },
+    component: () => import("@/views/parques/FichaParqueView.vue"),
+  },
+  {
+    path: "/ficha-tecnica/:idDoc",
+    name: "FichaSenlleira",
+    meta:{
+      title: 'Ficha da senlleira',
+    },
+    component: () => import("@/views/senlleira/FichaSenlleiraView.vue"),
   },
   
   // ADMIN
@@ -68,36 +104,5 @@ export const routes = [
     name: "admin-parque",
     component: () => import("@/views/admin/CatalogoParques.vue"),
   },
-  // test
-  {
-    path: "/catalogo-de-especies",
-    name: "CatalogoDeEspecies",
-    component: () => import("@/views/especies/CatalogoEspecies.vue"),
-  },
-  {
-    path: "/busqueda",
-    name: "Busqueda",
-    component: () => import("@/components/busqueda/Busqueda.vue"),
-  },
-  {
-    path: "/catalogo-de-parques",
-    name: "CatalogoDeParques",
-    component: () => import("@/views/parques/CatalogoParques.vue"),
-  },
-  {
-    path: "/ficha-parque/:idDoc",
-    name: "FichaParque",
-    component: () => import("@/views/parques/FichaParqueView.vue"),
-  },
-  {
-    path: "/ficha-tecnica/:idDoc",
-    name: "FichaSenlleira",
-    component: () => import("@/views/senlleira/FichaSenlleiraView.vue"),
-  },
-  {
-    path: "/prueba",
-    name: "prueba",
-    component: () => import("@/views/PruebasView.vue"),
-  }
 
 ];
