8ad2e9b104fa3c0a2e06193df6a71808a61ad49f
tanianeiro <tanitanv1992@gmail.com>
2022-07-21 14:18:30 +0200

    limpiado codigo de css del TheUploader no necesario

--- src/components/theUploader.vue
+++ src/components/theUploader.vue
@@ -27,29 +27,6 @@ const subirFoto = event => {
 
 </script>
 <style scoped>
-/* #subirImagen {
-    width: 0;
-    height: 0;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
-} */
-
-/* .subir-imagen-btn{
-    display: flex;
-    width: fit-content;
-    justify-content: center;
-    align-items: center;
-    margin:  auto;
-    cursor: pointer;
-    background-color: var(--colorclaro);
-    border-radius: 5px;
-    gap: .25rem;
-    padding: .5rem .75rem;
-    font-size: 1.1rem;
-    box-shadow: inset -1px -1px  5px 1px rgba(150,150,150, .7);
-} */
     .subir-imagen-btn svg{
         font-size: 1rem;
     }
