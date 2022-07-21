02a23119926cc64b51814c82a7ee63a2fd5d787b
martin <gagochorenmartin@gmail.com>
2022-07-21 14:20:51 +0200

    skeleton de la noticia

--- src/components/componentesGenerales/NoticiaCarballo.vue
+++ src/components/componentesGenerales/NoticiaCarballo.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="noticia-component">
+<div v-if="loading" class="noticia-component">
     <img src="../../assets/img/imagenes-portada/MIRADOR.jpg" alt="imagen mirador de conxo">
     <div class="noticia-block-txt">
         <h1>Carballo banquete de conxo</h1>
@@ -10,12 +10,27 @@
         }">Ver árbore</router-link>
     </div>
 </div>
+<SkeletonInicioVue v-else="loading"></SkeletonInicioVue>
 </template>
 
 <script setup>
 //Descomenta estas líneas si deseas encontrar el código en la BBDD del ID del árbol de conxo o búsca este id en firestore en su base de datos
 //import {busquedaDatos} from "../../hook/firestore.hook";
+import {ref} from 'vue';
+import SkeletonInicioVue from '../skeleton/SkeletonInicio.vue';
 import('@/assets/css/componente/noticia-arbol.css');
+
+const loading = ref(false)
+
+if(!loading.value) {
+setTimeout(() => {
+    loading.value = true;
+}, 400);
+}else{
+    loading.value = true
+}
+
+
 
 // (async()=>{
 //     const res = await busquedaDatos("Arbores","nombre_arbol","Carballo do Banquete de Conxo");
